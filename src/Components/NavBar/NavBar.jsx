b1f57755ef367dc814b48bd9d717d2a22d7f3c08
Alexia Falcone <alexiafalcone1995@gmail.com>
2023-11-02 09:37:44 -0300

    logo en png

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from '../CartWidget/CartWidget';
-import logo from '../../assets/logo.jpeg';
+import logo from '../../assets/logo.png';
 import './NavBar.css';
 
 
